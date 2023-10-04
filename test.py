5d7905ffbda271bb9391045a6daaacb3398f8616
vamsi krishna <vnarams@iu.edu>
2023-10-04 15:22:27 -0400

    Initial Commit

---
+++ test.py
@@ -0,0 +1,16 @@
+import unittest
+from app import scrape_and_load  
+from app import create_reactor_status_table
+
+class TestScrapeAndLoad(unittest.TestCase):
+
+    def test_scrape_and_load(self):
+        result = scrape_and_load()
+        # Assuming the function should return a non-empty list
+        self.assertTrue(result)  # This checks that the result is not None or an empty list
+    def test_create_reactor_status_table(self):
+        result = create_reactor_status_table
+        self.assertTrue(result)
+
+if __name__ == '__main__':
+    unittest.main()
